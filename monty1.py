a9f95f94c30bbc1f932863fd55fee9cb33ad1788
Ogochukwu Mukolo <ogochukwumukolo@gmail.com>
2022-03-13 20:23:35 +0000

    code to desmostrate input and concatenate.

---
+++ monty1.py
@@ -0,0 +1,4 @@
+name = input("What is your name.")
+quest = input("What is your Quest.")
+fav_color = input("What is your favourite color.")
+print("So your name is " + name +", your quest is " + quest +" and your favourite color is " + fav_color +".")
