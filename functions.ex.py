dc441fe0b2323a61c74b7abef0f31d7ee5388b90
Ogochukwu Mukolo <ogochukwumukolo@gmail.com>
2022-03-14 01:27:09 +0000

    code to show how to use the return keyword and parameters in functions.

---
+++ functions.ex.py
@@ -0,0 +1,20 @@
+# Volume of a prism
+
+# def vol(length=10, width=8, height=15):
+#     return length * width * height
+#
+#
+# print("The volume of the rectangular prism is " + str(vol()) + " cubic feet.")
+
+
+
+length = int(input("Enter an integer for length."))
+width = int(input("Enter an integer for with."))
+height = int(input("Enter an integer for height."))
+
+
+def vol(l, w, h):
+    return l * w * h
+
+
+print("The volume of the rectangular prism is " + str(vol(length, width, height)) + " cubic feet.")
